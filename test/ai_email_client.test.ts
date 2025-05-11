9b39f4f67227701820c536b70b89faa573c21c45
trey-rosius <treyrosius@gmail.com>
2025-05-12 00:25:33 +0100

    Initial commit

---
+++ test/ai_email_client.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiEmailClient from '../lib/ai_email_client-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai_email_client-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiEmailClient.AiEmailClientStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
